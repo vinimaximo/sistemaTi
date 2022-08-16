92ea01e0661cea3de9f4329f0d358b220c73f085
Vinicius Maximo Lima <vinimaximo033@hotmail.com>
2022-08-16 11:51:12 -0300

    telas de login

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,15 @@
 <?php
-
+// Incluindo o sistema de autenticação
+    include('acesso_comum.php')
 ?> 
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Área Administrativa</title>
+</head>
+<body>
+    <?php include('menu_adm.php')?>
+    <?php include('adm_options.php')?>
+</body>
+</html>
